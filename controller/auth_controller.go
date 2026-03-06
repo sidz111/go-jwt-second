5668e3298a3f32a07e5f60f7832739288add100d
sidz111 <sssurwade2212@gmail.com>
2026-03-07 00:39:09 +0530

    add middleware layer

---
+++ controller/auth_controller.go
@@ -0,0 +1,47 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	dbconfig "github.com/sidz111/jwt-second-project/dbConfig"
+	"github.com/sidz111/jwt-second-project/models"
+	"github.com/sidz111/jwt-second-project/utils"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func login(c *gin.Context) {
+	var user models.User
+	var foundUser models.User
+
+	err := c.BindJSON(&user)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid request body"})
+		return
+	}
+
+	err = dbconfig.DB.Where("id = ?", user.ID).First(&foundUser).Error
+	if err != nil {
+		c.JSON(401, gin.H{"error": "Invalid credentials"})
+		return
+	}
+	dbconfig.DB.Where("id = ?", user.ID).First(&foundUser)
+	if err := bcrypt.CompareHashAndPassword(
+		[]byte(user.Password),
+		[]byte(foundUser.Password),
+	); err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "Invalid Password",
+		})
+		return
+	}
+
+	token, err := utils.GenerateJWT(user.Name, int(user.ID))
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Failed to generate token"})
+		return
+	}
+
+	c.JSON(200, gin.H{"token": token})
+
+}
---
+++ middleware/auth_middleware.go
@@ -0,0 +1,39 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var SECRET_KEY = []byte("supersecretkey")
+
+func AuthMiddleware() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+
+		authHeader := c.GetHeader("Authorization")
+
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "No token"})
+			c.Abort()
+			return
+		}
+
+		tokenString := strings.Split(authHeader, " ")[1]
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return SECRET_KEY, nil
+		})
+
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
