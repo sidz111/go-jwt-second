4be8ab072ba1972c5c9abbf9bfe14096f3b2b7aa
sidz111 <sssurwade2212@gmail.com>
2026-03-06 17:58:23 +0530

    db config done

---
+++ dbConfig/db.go
@@ -0,0 +1,26 @@
+package dbconfig
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB(db *gorm.DB) error {
+	user_name := "root"
+	user_password := "root"
+	host := "localhost"
+	port := "3306"
+	db_name := "jwt_db_user"
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true", user_name, user_password, host, port, db_name)
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	DB = database
+	return nil
+}
